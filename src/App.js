d38c5309f01ee228d66b2288bced8f6363942565
coderdivya18 <coderdivya18@gmail.com>
2025-04-11 12:09:09 +0530

    Clean up unused images and update components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      HomePage
-    </div>
+    <>
+      <Header />
+      <Navbar />
+      <Main />
+      <Footer/>
+
+
+    </>
   );
 }
 
